eb2e80ed564bde23ca5892e1d8e6c2ead04b783f
obaydullahmhs <obaydullahmhs@gmail.com>
2020-09-16 15:10:03 +0600

    Update

--- Data Structures/segmentTree(class).cpp
+++ Data Structures/segmentTree(class).cpp
@@ -1,50 +1,125 @@
-class segment
-{
-public:
-    int tree[400015];
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+namespace Tree{
+    ///Start
+    template<typename T, typename Y>
+    class SegmentTree{
+    protected:
+        virtual T merge(T x, T y) = 0;
+        virtual T valueOf(Y a) = 0;
+        virtual T neutral() = 0;
+    private:
+        int n; vector<T> tree;
+        void build(const vector<Y> &given, int node, int low, int high){
 
-    void SegmentTree(vector<int> &given, int node, int low, int high)
-    {
-        if (low == high)
-        {
-            tree[node] = given[low];
-            return;
+            if (low == high){
+                tree[node] = valueOf(given[low]);
+                return;
+            }
+            int left = node<<1;
+            int right = left + 1;
+            int mid = (low + high)>>1;
+            build(given, left, low, mid);
+            build(given, right, mid + 1, high);
+            tree[node] = merge(tree[left], tree[right]);
         }
-        int left = 2 * node;
-        int right = 2 * node + 1;
-        int mid = (low + high) / 2;
-        SegmentTree(given, left, low, mid);
-        SegmentTree(given, right, mid + 1, high);
-        tree[node] = tree[left] + tree[right];
-    }
-    void update(int node, int low, int hi, int i, int value)
-    {
+        void update(int node, int low, int hi, int i, Y value){
 
-        if (low == hi)
-        {
-            tree[node] = value;
-            return;
+            if (low == hi){
+                tree[node] = valueOf(value);
+                return;
+            }
+            int mid = (low + hi)>>1;
+            int left = node<<1;
+            int right = left + 1;
+            if (i <= mid)
+                update(left, low, mid, i, value);
+            else
+                update(right, mid + 1, hi, i, value);
+            tree[node] = merge(tree[left], tree[right]);
         }
-        int mid = (low + hi) / 2;
-        int left = 2 * node;
-        int right = left + 1;
-        if (i <= mid)
-            update(left, low, mid, i, value);
-        else
-            update(right, mid + 1, hi, i, value);
-        tree[node] = tree[left] + tree[right];
-    }
-    int query(int node, int low, int hi, int i, int j)
-    {
-        if (i>hi || j<low)
+        T query(int node, int low, int hi, int i, int j){
+
+            if (i>hi || j<low)
+                return neutral();
+            if (low >= i&&hi <= j)
+                return tree[node];
+            int mid = (low + hi)>>1;
+            int left = node<<1;
+            int right = left + 1;
+
+            return merge(query(left, low, mid, i, j), query(right, mid + 1, hi, i, j)) ;
+        }
+    public:
+        SegmentTree(){}
+        void init(int _n){n = _n, tree.assign(4*n + 20, 0);} /// Must need to initialize
+        void build(const vector<Y>& given){
+            build(given, 1, 1, n);
+        }
+        void update(int ind, Y value){
+            update(1, 1, n, ind, value);
+        }
+        T query(int l, int r){
+            return query(1, 1, n, l, r);
+        }
+    };
+    ///End
+
+    template<typename T, typename Y>
+    class AddSegTree: public SegmentTree<T, Y>{
+        virtual T merge(T x, T y)override {
+            return x + y;
+        }
+        virtual T valueOf(Y x)override {
+            return T(x);
+        }
+        virtual T neutral()override{
             return 0;
-        if (low >= i&&hi <= j)
-            return tree[node];
-        int mid = (low + hi) / 2;
-        int left = 2 * node;
-        int right = left + 1;
-        int x = query(left, low, mid, i, j);
-        int y = query(right, mid + 1, hi, i, j);
-        return x + y;
+        }
+    };
+
+    template<typename T, typename Y>
+    class MinSegTree: public SegmentTree<T, Y>{
+        virtual T merge(T x, T y)override {
+            return min(x, y);
+        }
+        virtual T valueOf(Y x)override {
+            return T(x);
+        }
+        virtual T neutral()override {
+            return numeric_limits<Y> ::max()/2;
+        }
+    };
+
+    template<typename T, typename Y>
+    class MaxSegTree: public SegmentTree<T, Y>{
+        virtual T merge(T x, T y)override {
+            return max(x, y);
+        }
+        virtual T valueOf(Y x)override {
+            return T(x);
+        }
+        virtual T neutral()override {
+            return numeric_limits<Y> ::min()/2;
+        }
+    };
+}
+using namespace Tree;
+
+int main (){
+    int n, q; cin>>n>>q;
+    vector<ll> v(n);
+    for(ll& in: v) cin>>in; v.insert(v.begin(), 0);
+
+    AddSegTree<ll, ll> seg; seg.init(n); seg.build(v);
+    while(q--){
+        int c, ind; ll val; cin>>c>>ind>>val;
+        if(c==1){
+            seg.update(++ind, val);
+        }else cout<<seg.query(++ind, val)<<"\n";
+
     }
-};
+    return 0;
+}
+
